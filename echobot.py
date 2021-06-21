f7af33caff43ec92e04ca840760e4d8a607806fc
UlkaBo <ul.bozhko@gmail.com>
2021-06-21 15:00:32 +0300

    add echobot

--- echobot.py
+++ echobot.py
@@ -2,6 +2,8 @@
 # pylint: disable=C0116,W0613
 # This program is dedicated to the public domain under the CC0 license.
 
+# @UA_ituniver_bot
+
 from telegram.ext import (Updater, CommandHandler, MessageHandler,
                           Filters, CallbackQueryHandler,
                           ConversationHandler, CallbackContext)
@@ -27,13 +29,12 @@ firebaseConfig = {
 }
 firebase = pyrebase.initialize_app(firebaseConfig)
 
-
 # db = firebase.database()
 # auth = firebase.auth()
 storage = firebase.storage()
 
 
-token = "1753379890:AAGP2o4U-DJjX7X5OenBcswPVAoo3ckK56U"
+token = "1693343330:AAF5y4tbASN2pPCOHHzFPQJ7dUN_VdrW0sw"
 """
 Simple Bot to reply to Telegram messages.
 First, a few handler functions are defined. Then, those functions are passed to
@@ -52,10 +53,6 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
-
-
-# Define a few command handlers. These usually take the two arguments update and
-# context.
 
 keyboard_start = [
     [InlineKeyboardButton("Добавить запись", callback_data="add_record")],
@@ -76,52 +73,46 @@ keyboard_change_record = [
     [InlineKeyboardButton("Добавить заметку", callback_data="add_note")],
 ]
 
+keyboard_add_record = [["Добавить телефон"], ["Добавить день рождения"],
+                       ["Добавить адрес"], ["Добавить email"], ["Добавить заметку"]]
 
-def deserialize_users(url):
-    """using the path "path" reads the file with contacts"""
-    '''
-    with open(filename, 'a') as f:
-        f.write(x+'\n')
-    storage.child(cloudfilename).put(filename)
-    storage.child(cloudfilename).download('', filename)
-    with open(filename, 'r') as f:
-        x = f.read()
-    
-    '''
+markup_add_record = ReplyKeyboardMarkup(
+    keyboard_add_record, one_time_keyboard=True)
 
+'''
+name_func_add_record = dict(zip([el[0] for el in keyboard_add_record], [
+    add_phone, change_bd, change_address, add_email, add_note]))
+
+'''
+
+
+def deserialize_users(cloudfilename):
+
+    url = storage.child(cloudfilename).get_url(None)
     try:
         f = urllib.request.urlopen(url).read()
         addressbook = pickle.loads(f)
     except:
-        addressbook = ''
+        addressbook = AddressBook()
     return addressbook
 
 
-def serialize_users(addressbook, path):
-    """saves a file with contacts on the path (object pathlib.Path) to disk"""
+def serialize_users(addressbook, cloudfilename):
+    """saves a file with contacts on the path (str) to firebase"""
     f = pickle.dumps(addressbook)
-    # print(f)
-    storage.child(path).put(f)
+    storage.child(cloudfilename).put(f)
 
 
-def echo(update: Update, context: CallbackContext) -> None:
-    """Echo the user message."""
-    x = update.message.text
+def pre_open_contacts(update):
 
-    # Storage
-    # print(update)
-    # print(type(update.username))
-    # print(update.message.chat.username)
-    username = update.message.chat.username
-    id_user = update.message.chat.id
+    event = update.message or update.callback_query.message
+
+    username = event.chat.username
+    id_user = event.chat.id
 
     filename = f"contacts_{id_user}.txt"
     cloudfilename = f"users/{filename}"
-    url = storage.child(cloudfilename).get_url(None)
-    addressbook = deserialize_users(url) + '\n' + x
-    #print(url, addressbook)
-    serialize_users(addressbook, cloudfilename)
-    update.message.reply_text(addressbook)
+    return cloudfilename
 
 
 def start(update: Update, context: CallbackContext) -> None:
@@ -131,10 +122,6 @@ def start(update: Update, context: CallbackContext) -> None:
     #    fr'Hi {user.mention_markdown_v2()}\!',
     #    reply_markup=ForceReply(selective=True),
     # )
-    if update.message is None:
-        event = update.callback_query.message
-    else:
-        event = update.message
 
     reply_start = InlineKeyboardMarkup(keyboard_start)
 
@@ -143,11 +130,100 @@ def start(update: Update, context: CallbackContext) -> None:
     #    content1, reply_markup=ReplyKeyboardRemove(), parse_mode="Markdown")
     # если нужно удалить нижнюю клавиатуру reply_markup=ReplyKeyboardRemove()
     # event.reply_photo(url_photo)
+    event = update.message or update.callback_query.message
     event.reply_text(message_start, reply_markup=reply_start)
 
 
 def add_record(update: Update, context: CallbackContext) -> None:
+
+    update.callback_query.message.reply_text("Для начала введите имя ")
+    return 0
+
+
+def new_name(update: Update, context: CallbackContext) -> None:
+    cloudfilename = pre_open_contacts(update)
+    addressbook = deserialize_users(cloudfilename)
+
+    name = update.message.text
+    context.user_data['name'] = name
+    record = Record(name)
+    addressbook.add_record(record)
+
+    serialize_users(addressbook, cloudfilename)
+
+    update.message.reply_text(text='номер телефона ')
+
+    return 1
+
+
+def add_phone(update: Update, context: CallbackContext) -> None:
+    cloudfilename = pre_open_contacts(update)
+    addressbook = deserialize_users(cloudfilename)
+    name = context.user_data['name']
+    record = addressbook[name]
+    phone = update.message.text
+    result = record.add_phone(phone)
+
+    if isinstance(result, Exception):
+        update.message.reply_text(
+            text="в запись добавлен новый телефон: \n ")
+
+    serialize_users(addressbook, cloudfilename)
+
+    update.message.reply_text(
+        text='введите день рождения в формате дд-мм-гггг("ввод" - пропустить):  ')
+
+    return 2
+
+
+def change_bd(update: Update, context: CallbackContext) -> None:
+    cloudfilename = pre_open_contacts(update)
+    addressbook = deserialize_users(cloudfilename)
+    name = context.user_data['name']
+    record = addressbook[name]
+    birthday_str = update.message.text
+    if birthday_str:
+        result = record.add_birthday(birthday_str)
+        if isinstance(result, Exception):
+            update.message.reply_text(
+                text='в запись добавлен день рождения: \n ')
+    else:
+        update.message.reply_text(
+            text='абоненту день рождения не добавлен')
+
+    serialize_users(addressbook, cloudfilename)
+
+    update.message.reply_text(
+        text='введите день адрес("ввод" - пропустить):  ')
+
+    return 3
+
+
+def change_address(update: Update, context: CallbackContext) -> None:
     pass
+
+
+def change_email(update: Update, context: CallbackContext) -> None:
+    pass
+
+
+def add_email(update: Update, context: CallbackContext) -> None:
+    pass
+
+
+def add_note(update: Update, context: CallbackContext) -> None:
+    pass
+
+
+'''
+def user_choice(update: Update, context: CallbackContext) -> None:
+    print('============', context.user_data)
+    context.user_data['choice'] = update.message.text
+    call_func = name_func_add_record[update.message.text]
+    # call_func()
+
+    return 2
+'''
 
 
 def change_record(update: Update, context: CallbackContext) -> None:
@@ -177,30 +253,6 @@ def change_phone(update: Update, context: CallbackContext) -> None:
     pass
 
 
-def add_phone(update: Update, context: CallbackContext) -> None:
-    pass
-
-
-def change_bd(update: Update, context: CallbackContext) -> None:
-    pass
-
-
-def change_address(update: Update, context: CallbackContext) -> None:
-    pass
-
-
-def change_email(update: Update, context: CallbackContext) -> None:
-    pass
-
-
-def add_email(update: Update, context: CallbackContext) -> None:
-    pass
-
-
-def add_note(update: Update, context: CallbackContext) -> None:
-    pass
-
-
 def help_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /help is issued."""
     update.message.reply_text('Help!')
@@ -214,6 +266,19 @@ def main() -> None:
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
+    conv_handler_add_record = ConversationHandler(
+        entry_points=[CallbackQueryHandler(add_record, pattern="add_record")],
+        states={
+            0: [MessageHandler(Filters.text, new_name)],
+            1: [MessageHandler(Filters.text, add_phone)],
+            2: [MessageHandler(Filters.text, change_bd)],
+            3: [MessageHandler(Filters.text, change_address)],
+            4: [MessageHandler(Filters.text, add_email)],
+            5: [MessageHandler(Filters.text, add_note)]
+        },
+        fallbacks=[MessageHandler(Filters.text, new_name)],
+    )
+    dp.add_handler(conv_handler_add_record)
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help_command))
 
@@ -236,8 +301,8 @@ def main() -> None:
     dp.add_handler(CallbackQueryHandler(add_note, pattern="add_note"))
 
     # on non command i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(
-        Filters.text & ~Filters.command, echo))
+    # dp.add_handler(MessageHandler(
+    #    Filters.text & ~Filters.command, echo))
 
     # Start the Bot
     updater.start_polling()
@@ -250,3 +315,14 @@ def main() -> None:
 
 if __name__ == '__main__':
     main()
+
+"""using the path "path" reads the file with contacts"""
+'''
+    with open(filename, 'a') as f:
+        f.write(x+'\n')
+    storage.child(cloudfilename).put(filename)
+    storage.child(cloudfilename).download('', filename)
+    with open(filename, 'r') as f:
+        x = f.read()
+    
+    '''
